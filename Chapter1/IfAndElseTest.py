c217f838d884bbb5291b11219122535b04fdda2a
XuanRanDev <3135419729@qq.com>
2021-10-13 19:55:07 +0800

    Add if and else and elif test program.

---
+++ Chapter1/IfAndElseTest.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    age = int(input("请输入你的年龄：\n"))
+
+    if  age < 18 :
+        print('你还没有成年')
+    elif age == 18:
+        print("你刚好成年")
+    elif age == 19:
+        print("你成年一年了！")
+    else:
+        print("你已经不是小孩子了！")
+
