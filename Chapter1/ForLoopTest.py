4477a9e1b0b99a4f1c2d14ada12534b64ead9643
XuanRanDev <3135419729@qq.com>
2021-10-13 20:02:47 +0800

    Fix a syntax error caused by positional error in code

--- Chapter1/ForLoopTest.py
+++ Chapter1/ForLoopTest.py
@@ -1,10 +1,10 @@
-if __name__ == '__main__':
-
-'''
+"""
 For 循环测试
 
 其中range函数param 1 ： 开始数字 2： 终止（包头不包围） 3：step 步长
-'''
+"""
+
+if __name__ == '__main__':
     sum = 0
     for i in range(1, 101, 2):
         sum += i
