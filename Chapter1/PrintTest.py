0ef6814f8c7a9d6fb22a866e97404988d66d5f5d
XuanRanDev <3135419729@qq.com>
2021-10-13 19:37:10 +0800

    Add a test program for writer a word to txt file.

--- Chapter1/PrintTest.py
+++ Chapter1/PrintTest.py
@@ -3,3 +3,13 @@ b = 6
 print(a * b)
 
 print(a if a >= b else b)
+
+
+def Fileinit():
+    f = open("E:/PycharmProjects/PythonStudy/FileTest/1.txt","a+")
+    print("This is a file test program.",file =  f)
+    f.close()
+
+
+if __name__ == '__main__':
+    Fileinit()
