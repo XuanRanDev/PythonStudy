7264d60349d375c7eba4d1564cda8013a046f618
XuanRanDev <3135419729@qq.com>
2021-10-14 13:17:27 +0800

    Add String Test.

---
+++ Chapter1/StringTest.py
@@ -0,0 +1,14 @@
+def showStringSplieTest():
+    s = "Hello"
+    s2 = "World"
+
+    print(s + s2)
+    print(s * 3)
+    print(s[0:4])
+
+    print('e' in s)
+    print('e' not in s2)
+
+
+if __name__ == '__main__':
+    showStringSplieTest()
