00d917a6855d865ec154d43b91169e53c879c8b8
XuanRanDev <3135419729@qq.com>
2021-10-14 14:20:40 +0800

    Add random reverse homework.

--- Demo/RandomHomeWork.py
+++ Demo/RandomHomeWork.py
@@ -1,11 +1,28 @@
 import random
 
-if __name__ == '__main__':
-    ls = []
 
+def sortedTest():
+    ls = []
     while len(ls) < 10:
         temp = random.randint(1, 20)
         if temp not in ls:
             ls.append(temp)
 
     print(sorted(ls))
+
+
+def sortedTest2():
+    """逆向排序"""
+    ls = []
+    while len(ls) < 10:
+        temp = random.randint(1, 20)
+        if temp not in ls:
+            ls.append(temp)
+
+    print(sorted(ls, reverse=True))
+
+
+if __name__ == '__main__':
+    # sortedTest()
+    sortedTest2()
+
