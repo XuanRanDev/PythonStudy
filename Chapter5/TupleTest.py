bd098f8ffe45e1783e91d0464c57af763c854e04
XuanRanDev <3135419729@qq.com>
2021-10-15 20:10:59 +0800

    Add ChangeTupleTest method to TupleTest.py

--- Chapter5/TupleTest.py
+++ Chapter5/TupleTest.py
@@ -1,6 +1,39 @@
-if __name__ == '__main__':
-    """元组Test"""
+import random
+def addNewTuple():
     # 声明一个元组
     tuple1 = ("1", "2", "3", "4")
     print(tuple1)
     print(type(tuple1))
+
+
+def HasATupleTest():
+    # 当Tuple只有一个元素时，需要加上逗号
+    tuple1 = ("aaa")
+    print(type(tuple1))
+    tuple2 = ("aa", )
+    print(type(tuple2))
+
+
+def ChangeTupleTest():
+    # 元组内容是不能改变的，所以不能添加数据，需要用另一种方法
+    ls = []
+    tu = ()
+
+    for i in range(1,20,2):
+        if i not in ls:
+            ls.append(random.randint(1, 50))
+
+    # 使用类型转换
+    tu = tuple(ls)
+
+    print(tu)
+
+    print(type(tu))
+
+
+
+if __name__ == '__main__':
+    """元组Test"""
+    # ChangeTupleTest()
+    # HasATupleTest()
+    # addNewTuple()
