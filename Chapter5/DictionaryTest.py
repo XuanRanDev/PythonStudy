3b4b20b37c2ddcf9494c6f41f1e4c041344adb91
XuanRanDev <3135419729@qq.com>
2021-10-20 18:51:32 +0800

    Add DictChangeTest method for DictionaryTest.py

--- Chapter5/DictionaryTest.py
+++ Chapter5/DictionaryTest.py
@@ -9,6 +9,17 @@ def ListToDictTest():
     """将List转成字典，前提是List中元素是成对出现的"""
 
 
+def DictChangeTest():
+    """字典修改测试"""
+    dict1 = {}
+    dict1['name'] = 'XuanRan'
+    dict1['age'] = 19
+    print(dict1)
+    dict1['age'] = 15
+    print(dict1['age'])
+
+
 if __name__ == '__main__':
-    ListToDictTest()
+    DictChangeTest()
+    # ListToDictTest()
     # Dict1Test()
