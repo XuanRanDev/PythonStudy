19d478b8843abedfdb70a3360f6b624bc1dd2d62
XuanRanDev <3135419729@qq.com>
2021-10-22 23:17:15 +0800

    实现Numpy创建

---
+++ Chapter7/NumpyTest.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def NumpyTest():
+    """Numpy创建"""
+    array = np.array(
+        [
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+    )
+    print(array)
+
+
+if __name__ == '__main__':
+    NumpyTest()
