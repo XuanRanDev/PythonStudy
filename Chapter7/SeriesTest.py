dace5dc135b9f46724777c78c24ffed052f8f412
XuanRanDev <3135419729@qq.com>
2021-10-23 22:10:58 +0800

    Add SeriesFromDiect Method

--- Chapter7/SeriesTest.py
+++ Chapter7/SeriesTest.py
@@ -1,10 +1,26 @@
 import pandas as pd
 
+
 def SeriesBaseTest():
     """Series基本形式"""
-    s = pd.Series(['a', 'b' , 'c'])
+    s = pd.Series(['a', 'b', 'c'])
+    print(s)
+
+
+def SeriesIndexTest():
+    """Series支持多索引"""
+    s = pd.Series(['a', 'b', 'c'], index=[100, 200, 100])
+    print(s)
+
+
+def SeriesFromDiect():
+    """Series支持由字典转型而来"""
+    dic = {'1' : 1 , '2' : 2, '3' : 3}
+    s = pd.Series(dic)
     print(s)
 
 
 if __name__ == '__main__':
-    SeriesBaseTest()
+    SeriesFromDiect()
+    # SeriesIndexTest()
+    # SeriesBaseTest()
