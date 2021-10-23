d48132571bb46d0572087b35ff288a61a9fd5006
XuanRanDev <3135419729@qq.com>
2021-10-23 21:39:31 +0800

    添加矩阵乘法测试方法

--- Chapter7/NumpyCalculateTest.py
+++ Chapter7/NumpyCalculateTest.py
@@ -12,5 +12,18 @@ def Test():
     print("元素相乘:", arr1 ** 2)
 
 
+def JuZhenChengFaTest():
+    """矩阵乘法测试"""
+
+    arr3 = np.array([[1,2,3],[4,5,6]])
+    arr4 = np.ones([3,2],dtype='int64')
+
+    print(arr3)
+    print(arr4)
+    print("矩阵乘法运算：")
+    print(arr3.dot(arr4))
+
+
 if __name__ == '__main__':
-    Test()
+    JuZhenChengFaTest()
+    # Test()
