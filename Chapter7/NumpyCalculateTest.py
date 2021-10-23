96af8fa3032806725306582bb044ceb3b3196d05
XuanRanDev <3135419729@qq.com>
2021-10-23 21:51:14 +0800

    添加矩阵相关方法操作

--- Chapter7/NumpyCalculateTest.py
+++ Chapter7/NumpyCalculateTest.py
@@ -24,6 +24,20 @@ def JuZhenChengFaTest():
     print(arr3.dot(arr4))
 
 
+def JuZhenJiSuanTest():
+    """矩阵相关计算方法测试"""
+    arr3 = np.array([[1, 2, 3], [4, 5, 6]])
+    # axis=1代表每一行求和 =0代表每一列
+    print("求矩阵一行和：" + repr(np.sum(arr3, axis=1)))
+    print("求矩阵最大值:" + repr(np.max(arr3)))
+    print("求矩阵最小值：" + repr(np.min(arr3)))
+    print("求矩阵均值：" + repr(np.mean(arr3)))
+    print("求矩阵最大值下标：" + repr(arr3.argmax()))
+    print("求矩阵最小值下标:" + repr(arr3.argmin()))
+
+
+
 if __name__ == '__main__':
-    JuZhenChengFaTest()
+    # JuZhenJiSuanTest()
+    # JuZhenChengFaTest()
     # Test()
