77ab1c52ee3a00a318bc29f05ee9a903015fa646
XuanRanDev <3135419729@qq.com>
2021-11-03 22:01:12 +0800

    添加数据爬取

---
+++ Demo/BrushPageviews.py
@@ -0,0 +1,21 @@
+import requests
+import time
+
+
+def start():
+    url = "https://blog.xuanran.work/"
+
+    heards = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    }
+
+    for i in range(100):
+        body = requests.get(url, heards)
+        print(body.text)
+        print("已访问" + repr(i))
+        i = i + 1
+
+
+
+if __name__ == '__main__':
+    start()
