773837afd6f957965c98d88a22f9933489beaf84
XuanRanDev <3135419729@qq.com>
2021-11-11 19:02:26 +0800

    优化代码

--- Demo/JiuJiuChengFaBiao.py
+++ Demo/JiuJiuChengFaBiao.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     """99乘法表和格式化输出测试"""
+
     for i in range(1, 10):
         for j in range(1, i+1):
             print('%d * %d = %d \t' % (i, j, i*j), end='')
-
         print()
