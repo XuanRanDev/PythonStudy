9bf0017132131eed163f6d61ea78a93fbfed4a42
XuanRanDev <3135419729@qq.com>
2021-11-11 17:03:09 +0800

    Add TurtleTest.py

---
+++ Chapter1/TurtleTest.py
@@ -0,0 +1,14 @@
+import turtle
+
+if __name__ == '__main__':
+    turtle.pensize(4)
+    turtle.pencolor('red')
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+
+    turtle.mainloop()
