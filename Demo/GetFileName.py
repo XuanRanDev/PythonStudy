34400a2efc86dd168c1d0a1f88084b5472539a1e
XuanRanDev <3135419729@qq.com>
2021-11-12 22:16:05 +0800

    添加获取文件名的测试程序

---
+++ Demo/GetFileName.py
@@ -0,0 +1,10 @@
+def main(filename):
+    if '.' not in filename:
+        return "null"
+    pos = filename.rfind('.') # 从右边开始查找
+    return filename[pos + 1:]
+
+
+if __name__ == '__main__':
+    """返回给定文件名的文件类型"""
+    print(main("XuanRan.mp3"))
